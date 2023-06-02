d8bb1fa295e85cbf577e31e94c64c439c3f61825
Ryan Williams <ryan.williams93@aol.com>
2023-06-02 15:38:35 -0500

    more styling, volume feature added

--- src/App.js
+++ src/App.js
@@ -1,105 +1,101 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
+const arr = [
+  {
+    keyCode: 81,
+    text: "Q",
+    id: "Heater-1",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  },
+  {
+    keyCode: 87,
+    text: "W",
+    id: "Heater-2",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  },
+  {
+    keyCode: 69,
+    text: "E",
+    id: "Heater-3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+  {
+    keyCode: 65,
+    text: "A",
+    id: "Heater-4",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  },
+  {
+    keyCode: 83,
+    text: "S",
+    id: "Clap",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    keyCode: 68,
+    text: "D",
+    id: "Open-HH",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  },
+  {
+    keyCode: 90,
+    text: "Z",
+    id: "Kick-n-Hat",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  },
+  {
+    keyCode: 88,
+    text: "X",
+    id: "Kick",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+  {
+    keyCode: 67,
+    text: "C",
+    id: "Closed-HH",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
+];
+
 function App() {
-  // const [activeKey, setActiveKey] = useState('');
 
-  // useEffect(() => {
-  //   document.addEventListener('keydown', (event) => {
-  //     playSound(event.key.toUpperCase());
-  //   });
-  // }, []); 
-
-  const arr = [
-    {
-      keyCode: 81,
-      text: "Q",
-      id: "Heater-1",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-    },
-    {
-      keyCode: 87,
-      text: "W",
-      id: "Heater-2",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
-    },
-    {
-      keyCode: 69,
-      text: "E",
-      id: "Heater-3",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-    },
-    {
-      keyCode: 65,
-      text: "A",
-      id: "Heater-4",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-    },
-    {
-      keyCode: 83,
-      text: "S",
-      id: "Clap",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
-    },
-    {
-      keyCode: 68,
-      text: "D",
-      id: "Open-HH",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
-    },
-    {
-      keyCode: 90,
-      text: "Z",
-      id: "Kick-n-Hat",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-    },
-    {
-      keyCode: 88,
-      text: "X",
-      id: "Kick",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-    },
-    {
-      keyCode: 67,
-      text: "C",
-      id: "Closed-HH",
-      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-    }
-  ];
-
-  // function playSound(selector){
-  //   const audio = document.getElementById(selector)
-  //   audio.play();
-  //   setActiveKey(selector)
-  // }
+  const [volume, setVolume] = useState(1);  
 
   return (
+
+
     <div className="App">
       <div id='drum-machine'>
         <div id="display"></div>
         <div className='drum-pads'>
           {arr.map((clip) => (
-            // <div 
-            //   key={clip.src}
-            //   onClick={() => {
-            //     playSound(clip.text)
-            //   }} 
-            //   id={clip.src} className='drum-pad'>
-            //   {clip.text}
-            //   <audio className='clip' id={clip.text} src={clip.src} />
-            // </div>
-            <Pad key={clip.id} clip={clip}/>
+            <Pad key={clip.id} clip={clip} volume={volume}/>
           ))}
         </div>
-        
+        <br/>
+        <div className='volume text-center'>
+          <h4>Volume</h4>
+          <input 
+            onChange={(e) => 
+              setVolume(e.target.value)
+            }
+            type="range" 
+            step="0.01"
+            value={volume}
+            max="1"
+            min="0"
+            className='w-50'
+          ></input>
+        </div>
       </div>
     </div>
   );
 }
 
-function Pad({ clip }){
+function Pad({ clip, volume }){
 
-  // const [activeKey, setActiveKey] = useState('');
+  const [active, setActive] = useState(false);
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyPress);
@@ -117,9 +113,11 @@ function Pad({ clip }){
 
   const playSound = () => {
     const audio = document.getElementById(clip.text)
+    setActive(true);
+    setTimeout(() => setActive(false), 200);
+    audio.volume = volume;
     audio.currentTime = 0;
     audio.play();
-    // setActiveKey(selector)
   }
 
   return (
@@ -128,15 +126,12 @@ function Pad({ clip }){
       onClick={() => {
         playSound(clip.text)
       }} 
-      id={clip.src} className='drum-pad'>
+      id={clip.src} 
+      className={`drum-pad bg-secondary ${active && "bg-info"}`}
+      >
       {clip.text}
       <audio className='clip' id={clip.text} src={clip.src} />
     </div>
-
-//     <div className='drum-pad'>
-//       <audio className='clip' id={clip.text} src={clip.src} />
-//       {clip.text}
-//     </div>
   );
 }
 
